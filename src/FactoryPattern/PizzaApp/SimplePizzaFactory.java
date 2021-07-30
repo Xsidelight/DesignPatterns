e6df5f2581601b96a170181ef995b7531c72ab96
Xsidelight <toko.gogberaa@gmail.com>
2021-07-30 12:52:20 +0400

    created pizzaapp with simplefactory

---
+++ src/FactoryPattern/PizzaApp/SimplePizzaFactory.java
@@ -0,0 +1,13 @@
+package FactoryPattern.PizzaApp;
+
+public class SimplePizzaFactory {
+    public Pizza createPizza(String type) {
+        return switch (type) {
+            case "cheese" -> new CheesePizza();
+            case "pepperoni" -> new PepperoniPizza();
+            case "clam" -> new ClamPizza();
+            case "veggie" -> new VeggiePizza();
+            default -> null;
+        };
+    }
+}
